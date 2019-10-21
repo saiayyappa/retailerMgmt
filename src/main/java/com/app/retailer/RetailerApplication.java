584a835eb25b69425296f03c18df8cad8150767f
saiayyappa <saiayyappaor@gmail.com>
2019-10-21 08:44:36 +0530

    Updated sample datas

--- src/main/java/com/app/retailer/RetailerApplication.java
+++ src/main/java/com/app/retailer/RetailerApplication.java
@@ -25,11 +25,17 @@ public class RetailerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		service.createOrUpdateRetailer(new RetailerEntity("Dummy1", "Iowa", "US", "987352", "9876543210", "dummy1@gmail.com"));
-		service.createOrUpdateRetailer(new RetailerEntity("Dummy2", "Ohio", "US", "654321", "9876543210", "dummy2@gmail.com"));
-		service.createOrUpdateRetailer(new RetailerEntity("Dummy3", "Wisconsin", "US", "987456", "9876543210", "dummy3@gmail.com"));
-		service.createOrUpdateRetailer(new RetailerEntity("Dummy4", "Texas", "US", "657513", "9876543210", "dummy4@gmail.com"));
-		service.createOrUpdateRetailer(new RetailerEntity("Dummy5", "Missouri", "US", "325733", "9876543210", "dummy5@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Rasheed", "Surat", "India", "123456", "1234567890", "rasheed1@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Ganesh", "Chennai", "India", "234567", "9876543210", "ganesh678@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Sarah", "Chandigarh", "India", "345678", "9988776655", "sarah7@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Baskar", "Baroda", "India", "456789", "1122334455", "baskar123@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Tejas", "Vadodara", "India", "567890", "2233445566", "tejastejas@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Hameedha", "Mumbai", "India", "123234", "9922334455", "hameedha96@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Dipanjan", "Delhi", "India", "234345", "8977665544", "dipan7jan@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Fareeda", "Mysore", "India", "456567", "8766554430", "fareeda45@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Ramesh", "Bangalore", "India", "678789", "9988774433", "ramesh12@gmail.com"));
+		service.createOrUpdateRetailer(new RetailerEntity("Ang", "Assam", "India", "890987", "9944556677", "angbender@gmail.com"));
+		
 		logger.info("All retailers -> {}", service.getAllRetailers());
 	}
 }
